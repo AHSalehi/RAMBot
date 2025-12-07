93ea0e9c1871699dcc0d37d7c9469f662ed8e0a4
AHSalehi <salehamir708@gmail.com>
2025-12-07 11:03:57 +0330

    Styles and first handler are added

--- main.py
+++ main.py
@@ -15,3 +15,24 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher()
 
+
+keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="ثبت نام دور ها"), KeyboardButton(text="ارتباط با اعضا")],
+    ],
+    resize_keyboard=True,
+)
+
+
+# intro/welcome message
+
+WELCOME = (
+    "سلام./n"
+    "به ربات انجمن رباتیک و مکاترونیک دانشگاه نوشیروانی بابل خوش اومدی! /n"
+    "از طریق ربات می تونی با اعضا در ارتباط باشی و همینطور در دوره ها و کلاس هامون شرکت کنی !/n" 
+)
+
+@dp.message(CommandStart())
+async def handle_start(message: types.Message):
+    await message.answer(WELCOME, reply_markup= keyboard)
+    
